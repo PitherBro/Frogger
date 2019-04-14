d2e02db2f2efb86317e275c705683b7b525fef1e
helderma <47367930+helderma@users.noreply.github.com>
2019-04-13 23:55:56 -0500

    updated a few things
    Updated

---
+++ FroggerReplica/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void loadMain() { SceneManager.LoadScene("MainMenu"); }
+    public void loadHighScore() { SceneManager.LoadScene("HighScores"); }
+    public void loadGame() { SceneManager.LoadScene("Game"); }
+    public void loadGameOver() { SceneManager.LoadScene("End"); }
+    public void loadSettings() { SceneManager.LoadScene("Settings"); }
+}
---
+++ FroggerReplica/Assets/Scripts/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    public static float CarSpawnRate = .3f,
+                        CarMaxSpeed = 12f,
+                        CarMinSpeed = 8f;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+                
+        }
+    }
+
+}
